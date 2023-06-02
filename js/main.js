6bcef6c17fa975a7c9f94f811e3450baea5e2f08
Aliia <112251947+A1mzali@users.noreply.github.com>
2023-06-02 20:53:56 +0300

    js-02-fun

--- js/main.js
+++ js/main.js
@@ -5,79 +5,103 @@ const header = document.querySelector('.header')
 const form = document.querySelector('#form')
 const input = document.querySelector('#inputCity')
 
+function removeCard() {
+  // Удаляем предыдующую карточку
 
+  const prevCard = document.querySelector('.card');
+  if (prevCard)prevCard.remove();
+};
+
+function showError(errorMessage) {
+  // Отобразить карточку с ошибкой
+  const html = `<div class="card">${errorMessage}</div>`
+
+   // Отображаем карточку на странице
+  header.insertAdjacentHTML('afterend', html)
+
+}
+
+function showCard (name,country, temp_c, condition) {
+  // создаем разметку для карточки
+         const html = `<div class="card">
+              <h2 class="card__city">${name}
+              <span>${country}</span></h2>
+            
+              <div class="card__weather">
+                <div class="card__value">${temp_c}<sup>°c</sup><div>
+                <img src="./img/example.png" alt="WEATHER" class="card__img">
+              </div>
+            
+              <div class="card__desc">${condition}</div>
+
+            </div>`
+
+    // Отображаем карточку на странице
+    header.insertAdjacentHTML('afterend', html)
+
+}
+
+async function getWeather(city) {
+  // Адрес запроса
+   const url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`;
+   const response = await fetch(url);
+   const data =  await response.json();   
+} 
 
 // Слушаем отправку формы
-form.onsubmit = function (e) {
+form.onsubmit = async function (e) {
   // Отменяем отправку формы
   e.preventDefault();
 
 // Берем значение из инпута, обрезаем пробелы
   let city = input.value.trim();
 
-  // Делае запрос на сервер
-  // Адрес запроса
+  // Получаем данные с сервера
+  const data = await getWeather(city);
 
-  
-const url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`;
+  // Проверяем ошибку
 
-
-
+if (data.error) {
+  // Если есть ошибка -выводим его
+    removeCard()
+    showError(data.error.message);
+ } else {
+  // Если ошибки нет -выводим карту
+  // Отображаем полученные данные на карточке
+    removeCard()
+    showCard (
+          data.location.name, 
+          data.location.country,
+          data.current.temp_c,
+          data.current.condition.text
+          );
+        }  
 // Выполняем зарос
 
     fetch(url)
     .then((response) => response.json())
     .then((data) => {
-
-      
       console.log(data);
 
       // Проверяем ошибку
 
       if (data.error) {
         // Если есть ошибка -выводим его
+        removeCard()
+        showError(data.error.message);
 
-        // Удаляем предыдующую карточку
-        const prevCard = document.querySelector('.card');
-        if (prevCard)prevCard.remove();
-
-
-        // Отобразить карточку с ошибкой
-        const html = `<div class="card">${data.error.message}</div>`
-        
-        header.insertAdjacentHTML('afterend', html)
-
-
-      }else {
-
+        }else {
         // Если ошибки нет -выводим карту
         // Отображаем полученные данные на карточке
 
-        // Удаляем предыдующую карточку
-        const prevCard = document.querySelector('.card');
-        if (prevCard)prevCard.remove();
-      
+        removeCard()
 
-      // создаем разметку для карточки
-        const html = `<div class="card">
-                        <h2 class="card__city">${data.location.name}
-                        <span>${data.location.country}</span></h2>
-                    
-                        <div class="card__weather">
-                          <div class="card__value">${data.current.temp_c}<sup>°c</sup></div>
-                          <img src="./img/example.png" alt="WEATHER" class="card__img">
-                        </div>
-                    
-                        <div class="card__desc">${data.current.condition.text}</div>
-                      
-                      </div>`
-
-      // Отображаем карточку на странице
-
-      header.insertAdjacentHTML('afterend', html)
-
-      }
-
+        showCard (
+          data.location.name, 
+          data.location.country,
+          data.current.temp_c,
+          data.current.condition.text
+          );
+        }    
     });
-
 }
